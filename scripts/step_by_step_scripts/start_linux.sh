3677d0a8e07366adf9272d955b8468a5be845f42
Zhang Junyang <junyang@stu.pku.edu.cn>
2025-08-06 10:38:31 +0800

    Fix Linux test script

--- scripts/step_by_step_scripts/start_linux.sh
+++ scripts/step_by_step_scripts/start_linux.sh
@@ -5,16 +5,18 @@ set -ex
 CORE_COUNT=${1:-128}
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-INITRAMFS_DIR="$SCRIPT_DIR/../test/build/initramfs.cpio.gz"
-EXT2_IMG_DIR="$SCRIPT_DIR/../test/build/ext2.img"
+IMG_SRC_DIR="$SCRIPT_DIR/../../cortenmm-adv/test"
+INITRAMFS_PATH="$IMG_SRC_DIR/build/initramfs.cpio.gz"
+BENCH_IMG_PATH="$IMG_SRC_DIR/build/bench_data.img"
+
+if [ ! -f "$INITRAMFS_PATH" ] || [ ! -f "$BENCH_IMG_PATH" ]; then
+    make build -C $IMG_SRC_DIR
+fi
+
 LINUX_KERNEL="/root/linux-6.13.8/bzImage"
 
 # Disable unsupported ext2 features of Asterinas on Linux to ensure fairness
-mke2fs -F -O ^ext_attr -O ^resize_inode -O ^dir_index ${EXT2_IMG_DIR}
-
-pushd "$SCRIPT_DIR/.."
-make initramfs
-popd
+mke2fs -F -O ^ext_attr -O ^resize_inode -O ^dir_index ${BENCH_IMG_PATH}
 
 /usr/local/qemu/bin/qemu-system-x86_64 \
     --no-reboot \
@@ -24,8 +26,8 @@ popd
     -cpu host,migratable=off,-pcid,+x2apic \
     --enable-kvm \
     -kernel ${LINUX_KERNEL} \
-    -initrd ${INITRAMFS_DIR} \
-    -drive if=none,format=raw,id=x0,file=${EXT2_IMG_DIR} \
+    -initrd ${INITRAMFS_PATH} \
+    -drive if=none,format=raw,id=x0,file=${BENCH_IMG_PATH} \
     -device virtio-blk-pci,bus=pcie.0,addr=0x6,drive=x0,serial=vext2,disable-legacy=on,disable-modern=off,queue-size=64,num-queues=1,config-wce=off,request-merging=off,write-cache=off,backend_defaults=off,discard=off,event_idx=off,indirect_desc=off,ioeventfd=off,queue_reset=off \
     -drive if=none,format=raw,id=x2,file=./test/build/bench_data.img \
     -device virtio-blk-pci,bus=pcie.0,addr=0x7,drive=x2,serial=vbench,disable-legacy=on,disable-modern=off,queue-size=64,num-queues=1,config-wce=off,request-merging=off,write-cache=off,backend_defaults=off,discard=off,event_idx=off,indirect_desc=off,ioeventfd=off,queue_reset=off \
