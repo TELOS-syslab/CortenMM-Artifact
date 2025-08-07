1555d65370e38fa1fd06acc0fbdbb99ec96b7909
Zhang Junyang <junyang@stu.pku.edu.cn>
2025-08-07 11:19:31 +0800

    Fix cpu numbers limiting

--- scripts/step_by_step_scripts/macrojvm.sh
+++ scripts/step_by_step_scripts/macrojvm.sh
@@ -28,12 +28,13 @@ fi
 BENCH_OUTPUT_FILE="macrojvm_${SYS_NAME}_$(date +%Y%m%d%H%M%S).log"
 
 THREAD_COUNTS=(1 2 4 8 16 32 64 128 192 256 320 384)
+NUM_HOST_CPUS=$(nproc)
 for THREAD_COUNT in "${THREAD_COUNTS[@]}"; do
-    if [ $THREAD_COUNT -eq 320 ]; then
-        export NR_CPUS=384
-    else
-        export NR_CPUS=$THREAD_COUNT
+    if [ $THREAD_COUNT -gt $NUM_HOST_CPUS ]; then
+        echo "$THREAD_COUNT is greater than the number of host CPUs ($NUM_HOST_CPUS), skipping..."
+        continue
     fi
+    export NR_CPUS=$THREAD_COUNT
     export CORTEN_RUN_ARGS="FEATURES=mprotect_async_tlb"
     $BENCH_SCRIPT $SYS_NAME $BENCH_OUTPUT_FILE "/test/corten_benchjvm.sh $THREAD_COUNT $DO_ASTER_BREAKDOWN"
 done
